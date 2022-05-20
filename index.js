3f4ba8885631869e3cd9cd717c836e9770b1a7bd
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-05-20 18:36:25 +0300

    past

--- index.js
+++ index.js
@@ -18,15 +18,45 @@ const game = {    //обьект сдержащий всю игру, все ег
   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
-  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
-  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
-  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',],
-  ]
+  ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'x',],
+  ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'o', 'x',],
+  ['o', 'o', 'o', 'o', 'x', 'x', 'o', 'o', 'x', 'x',],
+  ],
+
+  activeTetromino: {  //активная фигура    : так как бьект
+    x: 3,
+    y: 0,
+    block: [
+      ['o','x','o',],
+      ['o','x','o',],
+      ['x','x','o',],
+    ]
+  },
+
+  moveLeft() {
+    this.activeTetromino.x -= 1;
+  },
+
+  moveRight() {
+    this.activeTetromino.x += 1;
+  },
+
+  moveDown() {
+    this.activeTetromino.y -= 1;
+  },
+
+  rotateTetromino() {
+
+  },
+
+  viewArea() { //метод который будет возвращать арея(копирвать его?)
+    const area = JSON.parse(JSON.stringify(this.area));  //скопировал, сначала сделал его стройкой , потом снва бьектов- анешним json.parse
+  }
 }
 
 
 //--------------------отрисовка------------------
-const SIZE_BLOCK = 30; //колонка
+const SIZE_BLOCK = 40; //колонка
 const container = document.querySelector('.container');
 
 const canvas = document.createElement('canvas');
@@ -37,11 +67,25 @@ canvas.height = SIZE_BLOCK * 20;
 
 const context = canvas.getContext('2d');//метод отрисовки создается один раз
 
-const showArea = area => {
-  console.log(area[0].length);
 
-  context.fillStyle = 'tomato';
-  context.fillRect(50, 100, 100, 60); //метод fillRect чтото оттисовать по x (коорX, коорX, ширина, высота) rect-прямоугольник
+  const showArea = area => {
+  for (let y = 0; y < area.length; y++) {
+      const line = area[y];
+      
+      for (let x = 0; x < line.length; x++) {
+          const block = line[x];
+          if (block === 'x') {
+          context.fillStyle = 'tomato';
+          context.strokeStyle = 'white';
+          context.fillRect(x * SIZE_BLOCK, y  * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK);
+          context.strokeRect(x * SIZE_BLOCK, y  * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK);
+          }
+      }
+      
+  }
+      console.log(area.length);
+
+ //метод fillRect чтото оттисовать по x (коорX, коорX, ширина, высота) rect-прямоугольник
 
 }; //функция которая будет отрисовывать поле канвас
 
